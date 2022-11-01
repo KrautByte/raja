c93c67446d9f517cdde80654b61c1d316e66f9e1
Lars Erhardt <lars@krautbyte.com>
2022-11-01 22:12:41 +0100

    Added Task and Resources
    Signed-off-by: Lars Erhardt <lars@krautbyte.com>

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# raja
+# Raja
 Apprentice Management Platform based on Laravel, Vue.js and TailwindCSS
--- routes/web.php
+++ routes/web.php
@@ -16,16 +16,9 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
-
-Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+Route::resource('tasks', 'App\Http\Controllers\TaskController');
+
+require __DIR__ . '/auth.php';
